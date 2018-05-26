4e1d241df6b92acf6d8920efe11ea937a98c1355
GamilM <GamilM@E0366482.internal.vodafone.com>
2018-05-26 21:56:57 +0200

    @first commit

---
+++ JenkinsSelenium/src/test/java/testpack/TC_001.java
@@ -0,0 +1,20 @@
+package testpack;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	@Test 
+	public void tc_003()
+	{
+		
+		assertTrue(true);
+	}
+	@Test 
+	public void tc_002()
+	{
+		
+		assertTrue(true);
+	}
+}
